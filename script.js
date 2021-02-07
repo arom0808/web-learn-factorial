853342cb41869b9915fc51422170a21962a2e169
Roman <arom0808@mail.ru>
2021-02-07 15:28:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function factorial(num){
+	if(num>=-1&&num<=1)
+		return(num);
+	else
+		if (num>0)
+			return(num*factorial(num-1));
+		else
+			return(num*factorial(num+1));
+}
+function numberOfZerosAtTheEnd(num){
+	num=num.toString()
+	let i = num.length - 1;
+  	while (i >= 0 && num.charAt(i) === '0') --i;
+  	return(num.length - 1 -i);
+}
+function writeFactorial(){
+	let res=factorial(parseInt(document.getElementById("valueInput").value));
+	console.log(res);
+	document.getElementById("result").innerHTML="result: "+res;
+	document.getElementById("numberOfZerosAtTheEnd").innerHTML=
+		"Number of zeros at the end: "+numberOfZerosAtTheEnd(res);
+}
