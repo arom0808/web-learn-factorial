e89d65d1425e8c887e941188fa1e7d8ee7815658
Roman <arom0808@mail.ru>
2021-02-07 21:11:47 +0300

    solved the problem with large numbers

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ function factorial(num){
 		return(num);
 	else
 		if (num>0)
-			return(num*factorial(num-1));
+			return(num*factorial(num-BigInt(1)));
 		else
-			return(num*factorial(num+1));
+			return(num*factorial(num+BigInt(1)));
 }
 function numberOfZerosAtTheEnd(num){
-	num=num.toString()
+	num=num.toString();
 	let i = num.length - 1;
   	while (i >= 0 && num.charAt(i) === '0') --i;
   	return(num.length - 1 -i);
 }
 function writeFactorial(){
-	let res=factorial(parseInt(document.getElementById("valueInput").value));
-	console.log(res);
-	document.getElementById("result").innerHTML="result: "+res;
+	let res=factorial(BigInt(document.getElementById("valueInput").value));
+	document.getElementById("result").innerHTML="result: "+res.toString();
 	document.getElementById("numberOfZerosAtTheEnd").innerHTML=
-		"Number of zeros at the end: "+numberOfZerosAtTheEnd(res);
+		"Number of zeros at the end: "+numberOfZerosAtTheEnd(res).toString();
 }
